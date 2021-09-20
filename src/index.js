20b9d7da09358da8d8a708dcbd74ddce81de586e
Engineerz <olaffix.biznes@gmail.com>
2021-09-20 19:35:18 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+function dimissBtn() {
+    var footer = document.getElementById("footer");
+    var dimissBtn = document.getElementById("dimissBtn");
+    var copyright = document.getElementById("copyright");
+
+    footer.classList.toggle('footer-hide');
+    dimissBtn.style.display = "none";
+    copyright.style.display = "none";
+}
+
+var leftBtn = document.getElementById("left");
+var rightBtn = document.getElementById("right");
+
+function btnLock(ms) {
+    leftBtn.disabled = true;
+    rightBtn.disabled = true;
+    setTimeout(function(){
+        leftBtn.disabled = false;
+        rightBtn.disabled = false;
+    },ms);
+}
+
+var slides = document.getElementById("inner");
+var first = document.getElementById("slide-1");
+var last = document.getElementById(`slide-${document.querySelectorAll(".bullets .slider").length}`);
+var current = 1;
+
+
+
+function left() {
+    if(!last.checked) {
+        slides.style.marginLeft = slides.offsetLeft -970 + "px";
+
+        current++;
+        var radio = document.getElementById(`slide-${current}`);
+        radio.checked = true;
+    }
+    else {
+        slides.style.marginLeft = slides.offsetLeft + (910 * (document.querySelectorAll(".bullets .slider").length - 1)) + "px";
+        current--;
+        var radio = document.getElementById(`slide-1`);
+        radio.checked = true;
+    }
+
+    btnLock(800);
+}
+
+function right() {
+    if(!first.checked) {
+        slides.style.marginLeft = slides.offsetLeft + 910 + "px";
+
+        current--;
+        var radio = document.getElementById(`slide-${current}`);
+        radio.checked = true;
+    }
+    else {
+        slides.style.marginLeft = slides.offsetLeft + (-970 * (document.querySelectorAll(".bullets .slider").length - 1)) + "px";
+        current++;
+        var radio = document.getElementById(`slide-${document.querySelectorAll(".bullets .slider").length}`);
+        radio.checked = true;
+    }
+
+    btnLock(800);
+}
---
+++ src/themes.js
@@ -0,0 +1,33 @@
+function changetheme() {
+    var theme = document.querySelector("#theme");
+    var themeswitch = document.getElementById("themeswitch");
+
+    var logo = document.getElementById("logo");
+    var mywork = document.getElementById("mywork")
+    var git = document.getElementById("git");
+    var stm = document.getElementById("stm");
+    var dsc = document.getElementById("dsc");
+    var leftBtn = document.getElementById("left");
+    var rightBtn = document.getElementById("right");
+
+    if (!themeswitch.checked) {
+        theme.href = "index-dark.css"
+        logo.src = "../res/engineerzdev.comlogocolor.svg"
+        mywork.src = "../res/myworkcolor.svg"
+        git.src = "../res/githubiconcolor.svg"
+        stm.src = "../res/steamlogocolor.svg"
+        dsc.src = "../res/discordlogocolor.svg"
+        leftBtn.style.background = "url(../res/chevron-left-color.svg)"
+        rightBtn.style.background = "url(../res/chevron-right-color.svg)"
+    }
+    else {
+        theme.href = "index-light.css"
+        logo.src = "../res/engineerzdev.comlogo.svg"
+        mywork.src = "../res/mywork.svg"
+        git.src = "../res/githubicon.svg"
+        stm.src = "../res/steamlogo.svg"
+        dsc.src = "../res/discordlogo.svg"
+        leftBtn.style.background = "url(../res/chevron-left.svg)"
+        rightBtn.style.background = "url(../res/chevron-right.svg)"
+    }
+}
