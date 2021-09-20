c1544128eddeb83cbd23057dfaa618ecabab16d8
Engineerz <50804794+EngineerzDev@users.noreply.github.com>
2021-09-20 20:03:21 +0200

    Update themes.js

--- themes.js
+++ themes.js
@@ -12,22 +12,22 @@ function changetheme() {
 
     if (!themeswitch.checked) {
         theme.href = "index-dark.css"
-        logo.src = "../res/engineerzdev.comlogocolor.svg"
-        mywork.src = "../res/myworkcolor.svg"
-        git.src = "../res/githubiconcolor.svg"
-        stm.src = "../res/steamlogocolor.svg"
-        dsc.src = "../res/discordlogocolor.svg"
-        leftBtn.style.background = "url(../res/chevron-left-color.svg)"
-        rightBtn.style.background = "url(../res/chevron-right-color.svg)"
+        logo.src = "./res/engineerzdev.comlogocolor.svg"
+        mywork.src = "./res/myworkcolor.svg"
+        git.src = "./res/githubiconcolor.svg"
+        stm.src = "./res/steamlogocolor.svg"
+        dsc.src = "./res/discordlogocolor.svg"
+        leftBtn.style.background = "url(./res/chevron-left-color.svg)"
+        rightBtn.style.background = "url(./res/chevron-right-color.svg)"
     }
     else {
         theme.href = "index-light.css"
-        logo.src = "../res/engineerzdev.comlogo.svg"
-        mywork.src = "../res/mywork.svg"
-        git.src = "../res/githubicon.svg"
-        stm.src = "../res/steamlogo.svg"
-        dsc.src = "../res/discordlogo.svg"
-        leftBtn.style.background = "url(../res/chevron-left.svg)"
-        rightBtn.style.background = "url(../res/chevron-right.svg)"
+        logo.src = "./res/engineerzdev.comlogo.svg"
+        mywork.src = "./res/mywork.svg"
+        git.src = "./res/githubicon.svg"
+        stm.src = "./res/steamlogo.svg"
+        dsc.src = "./res/discordlogo.svg"
+        leftBtn.style.background = "url(./res/chevron-left.svg)"
+        rightBtn.style.background = "url(./res/chevron-right.svg)"
     }
 }
