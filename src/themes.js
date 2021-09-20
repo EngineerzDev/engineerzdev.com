7219f82f2a7d206b2f860d3bc16696e6cb1da746
Engineerz <olaffix.biznes@gmail.com>
2021-09-20 20:15:28 +0200

    fixed the button image stacking visual error

--- src/themes.js
+++ src/themes.js
@@ -19,6 +19,8 @@ function changetheme() {
         dsc.src = "../res/discordlogocolor.svg"
         leftBtn.style.background = "url(../res/chevron-left-color.svg)"
         rightBtn.style.background = "url(../res/chevron-right-color.svg)"
+        leftBtn.style.backgroundRepeat = "no-repeat"
+        rightBtn.style.backgroundRepeat = "no-repeat"
     }
     else {
         theme.href = "index-light.css"
