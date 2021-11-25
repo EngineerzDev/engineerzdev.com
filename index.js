06435d2c1d0d970b9443e60b8425a2c4848cd2ba
Engineerz <50804794+EngineerzDev@users.noreply.github.com>
2021-11-25 20:24:42 +0100

    Added a special easteregg

--- index.js
+++ index.js
@@ -62,3 +62,9 @@ function right() {
 
     btnLock(800);
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.altKey && event.key === 'r') {
+      window.location.href="https://youtu.be/dQw4w9WgXcQ"
+    }
+});
